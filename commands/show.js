81b3cb84580ca157a3b10263d9d93977319356db
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-04-09 15:41:48 +0700

    feat(cmd): add show command

---
+++ commands/show.js
@@ -0,0 +1,22 @@
+const knex = require("../database/connect");
+
+module.exports = {
+  name: "show",
+  description: "Show bot's config",
+  args: ["settings"],
+  run: (client, target, context, args) => {
+    const [settingsName] = args;
+
+    knex("toddsbinConfig")
+      .where({
+        settingsName,
+      })
+      .then(([rows]) => {
+        if (!rows) {
+          return client.say(target, `ไม่พบการตั้งค่า ${settingsName}`);
+        }
+        client.say(target, `${settingsName} ถูกตั้งไว้เป็น ${rows.settingsValue}`);
+      })
+      .catch(console.error);
+  },
+};
